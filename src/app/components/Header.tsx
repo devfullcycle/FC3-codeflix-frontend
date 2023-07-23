15ce7c17625d60b53e9b63601bbcb26a46654801
Vitor Alencar <vitor.maia.alencar@gmail.com>
2023-07-23 07:37:18 +0200

    chore: separation of concerns

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,16 +1,12 @@
 'use client';
 import React, { useEffect, useState } from 'react';
 
-export default function Header() {
+const useScroll = () => {
   const [isScrolled, setIsScrolled] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY > 0) {
-        setIsScrolled(true);
-      } else {
-        setIsScrolled(false);
-      }
+      setIsScrolled(window.scrollY > 0);
     };
 
     window.addEventListener('scroll', handleScroll);
@@ -20,34 +16,45 @@ export default function Header() {
     };
   }, []);
 
+  return isScrolled;
+};
+
+const Logo = () => (
+  <img src='https://rb.gy/ulxxee' alt='netflix' width={90} height={90} />
+);
+
+const NavLinks = () => (
+  <nav>
+    <ul className='hidden md:flex md:space-x-4'>
+      <li>Home</li>
+      <li>TV Shows</li>
+      <li>Movies</li>
+      <li>Latest</li>
+    </ul>
+  </nav>
+);
+
+const UserProfile = () => (
+  <div className='flex items-center space-x-4'>
+    <p className='hidden cursor-not-allowed lg:inline'>Kids</p>
+    <img src='https://rb.gy/g1pwyx' alt='' className='cursor-pointer rounded' />
+  </div>
+);
+
+export default function Header() {
+  const isScrolled = useScroll();
+
   return (
     <header
-      className={`
-    ${isScrolled && 'bg-black'}
-    fixed top-0 z-50 flex w-full items-center justify-between  px-4 py-4 transition-all lg:px-10 lg:py-6 `}
+      className={`${
+        isScrolled && 'bg-black'
+      } fixed top-0 z-50 flex w-full items-center justify-between p-2 transition-all lg:px-10 lg:py-4`}
     >
       <div className='flex items-center space-x-2 md:space-x-8'>
-        <img
-          src='https://rb.gy/ulxxee'
-          alt='netflix'
-          width={120}
-          height={120}
-        />
-        <ul className='hidden md:flex md:space-x-4'>
-          <li>Home</li>
-          <li>TV Shows</li>
-          <li>Movies</li>
-          <li>Latest</li>
-        </ul>
+        <Logo />
+        <NavLinks />
       </div>
-      <div className='flex items-center space-x-4'>
-        <p className='hidden cursor-not-allowed lg:inline'>Kids</p>
-        <img
-          src='https://rb.gy/g1pwyx'
-          alt=''
-          className='cursor-pointer rounded'
-        />
-      </div>
+      <UserProfile />
     </header>
   );
 }
