dae9389d457bcb77e1eef2b295e239408c3aa744
Vitor Alencar <vitor.maia.alencar@gmail.com>
2023-07-23 07:36:23 +0200

    chore: page cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import Image from 'next/image';
-import { Inter } from 'next/font/google';
 import Header from './components/Header';
-const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
