b9961ec88847b4a4491ebd850abc2c3d0e31380d
Vitor Alencar <vitor.maia.alencar@gmail.com>
2023-08-27 20:31:36 +0200

    chore: updating page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,16 @@
-import Image from 'next/image';
+import { Banner } from './components/Banner';
 import Header from './components/Header';
+import { MovieRow } from './components/MovieRow';
 
 export default function Home() {
   return (
-    <div className='relative h-screen overflow-hidden bg-gradient-to-b lg:h-[140vh]'>
+    <div className='relative bg-gradient-to-b pb-8'>
       <Header />
-      <main className='relative pb-24 pl-4 lg:pl-16'>
-        <div className='flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12'>
-          <div className='absolute left-0 top-0 -z-10 flex h-[95vh] w-screen flex-col bg-black'>
-            <Image
-              src='/banner.png'
-              alt='MAID'
-              fill={true}
-              className='h-[65vh] object-cover object-top lg:h-[95vh]'
-            />
-          </div>
-
-          <h1 className='text-2xl font-bold md:text-4xl lg:text-7xl'>MAID</h1>
-
-          <p className='text-shadow-md max-w-xs text-xs md:max-w-lg md:text-lg lg:max-w-2xl'>
-            After fleeing an abusive relationship, a young mother finds a job
-            cleaning houses as she fights to provide for her child and build
-            them a better future.
-          </p>
-        </div>
-
-        <div className='flex space-x-3'>
-          <button className='md:text-xl; flex cursor-pointer items-center gap-x-2 rounded bg-white px-5 py-1.5 text-sm font-semibold text-black transition hover:opacity-75 md:px-8 md:py-2.5'>
-            Play
-          </button>
-          <button className='md:text-xl; flex cursor-pointer items-center gap-x-2 rounded bg-gray-600 px-5 py-1.5 text-sm font-semibold text-black transition hover:opacity-75 md:px-8 md:py-2.5'>
-            More Info
-          </button>
-        </div>
+      <main className='relative h-screen mb-48 pl-4 lg:pl-16 '>
+        <Banner />
+        <MovieRow sectionTitle='Popular' />
+        <MovieRow sectionTitle='Trending' />
+        <MovieRow sectionTitle='Top Rated' />
       </main>
     </div>
   );
