ae912988afdbf6f76dcab4d77dd9158bc4dcac44
Vitor Alencar <vitor.maia.alencar@gmail.com>
2023-08-27 20:30:42 +0200

    fix: header style changes

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -12,7 +12,7 @@ export default function Header() {
     <header
       className={`${isScrolled && 'bg-black'}
       fixed top-0 z-50
-      flex w-full items-center justify-between bg-gradient-to-t from-transparent to-black p-2 transition-all lg:px-10 lg:py-4`}
+      flex w-full items-center justify-between bg-gradient-to-t from-transparent to-black p-2 px-4 transition-all lg:px-16 lg:py-4`}
     >
       <div className='flex items-center space-x-2 md:space-x-8'>
         <Logo />
