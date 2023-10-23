e37555edb951ec2af3c6d3035ac5ff8f581ae8b3
Vitor Alencar <vitor.maia.alencar@gmail.com>
2023-10-23 22:59:31 +0200

    chore: updating components

---
+++ src/app/components/AuthForm.tsx
@@ -0,0 +1,66 @@
+'use client';
+
+import { InputField } from './InputField';
+
+type AuthFormProps = {
+  formType: 'login' | 'register';
+  onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+};
+
+export const AuthForm: React.FC<AuthFormProps> = ({ formType, onSubmit }) => {
+  return (
+    <form
+      onSubmit={onSubmit}
+      className='flex w-full max-w-md flex-col space-y-4 rounded bg-[#141414] bg-opacity-90 px-4 py-8 shadow-lg'
+    >
+      <div className='flex flex-col items-center space-y-4'>
+        <h1 className='text-3xl font-bold'>
+          {formType === 'login' ? 'Login' : 'Register'}
+        </h1>
+        <p className='text-sm text-gray-500'>
+          {formType === 'login'
+            ? 'New to the app?'
+            : 'Already have an account?'}{' '}
+          <a
+            href={formType === 'login' ? '/register' : '/login'}
+            className='text-red-500 hover:underline'
+          >
+            {formType === 'login' ? 'Register' : 'Login'}
+          </a>
+        </p>
+      </div>
+      <div className='mt-8 flex flex-col space-y-4'>
+        <InputField
+          id='email'
+          type='email'
+          label='Email'
+          placeholder='Enter your email'
+        />
+
+        <InputField
+          id='password'
+          type='password'
+          label='Password'
+          placeholder='Enter your password'
+        />
+
+        {formType === 'register' && (
+          <InputField
+            id='confirmPassword'
+            type='password'
+            label='Confirm Password'
+            placeholder='Confirm your password'
+          />
+        )}
+      </div>
+      <div className='flex flex-col-reverse space-y-2 pt-2 sm:flex-row sm:space-x-2 sm:space-y-0'>
+        <button
+          className='flex w-full items-center justify-center rounded-lg bg-red-500 px-4 py-2 text-sm font-semibold text-white hover:bg-red-600 sm:w-auto sm:px-8'
+          type='submit'
+        >
+          {formType === 'login' ? 'Login' : 'Register'}
+        </button>
+      </div>
+    </form>
+  );
+};
--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,9 +4,23 @@ import { useScroll } from '../hooks/useScroll';
 import { Logo } from './Logo';
 import { NavLinks } from './NavLinks';
 import { UserProfile } from './UserProfile';
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useRouter, useSearchParams } from 'next/navigation';
+import Link from 'next/link';
 
 export default function Header() {
   const isScrolled = useScroll();
+  const router = useRouter();
+  const params = useSearchParams();
+  const [searchTerm, setSearchTerm] = React.useState(params.get('title'));
+
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const data = new FormData(e.currentTarget);
+    const searchTerm = data.get('search') as string;
+    setSearchTerm(searchTerm);
+    router.push(`/search?title=${encodeURIComponent(searchTerm)}`);
+  };
 
   return (
     <header
@@ -15,10 +29,28 @@ export default function Header() {
       flex w-full items-center justify-between bg-gradient-to-t from-transparent to-black p-2 px-4 transition-all lg:px-16 lg:py-4`}
     >
       <div className='flex items-center space-x-2 md:space-x-8'>
-        <Logo />
+        <Link href='/'>
+          <Logo />
+        </Link>
         <NavLinks />
       </div>
-      <UserProfile />
+      <div className='flex items-center space-x-2'>
+        <form onSubmit={handleSearch} className='flex items-center space-x-2'>
+          <button type='submit'>
+            <MagnifyingGlassIcon className='h-6 w-6 text-gray-400' />
+          </button>
+          <input
+            type='search'
+            id='search'
+            name='search'
+            placeholder='Search'
+            value={searchTerm || ''}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            className='bg-transparent text-white placeholder-white outline-none'
+          />
+        </form>
+        <UserProfile />
+      </div>
     </header>
   );
 }
