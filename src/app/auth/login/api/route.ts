c4404c3226828e4a51146efd165c5e343e350d94
Vitor Alencar <vitor.maia.alencar@gmail.com>
2023-10-29 15:47:09 +0100

    chore: removing logers

--- src/app/auth/login/api/route.ts
+++ src/app/auth/login/api/route.ts
@@ -5,10 +5,8 @@ import { NextRequest, NextResponse } from 'next/server';
 export async function POST(request: NextRequest) {
   try {
     const { email, password } = loginFormSchema.parse(await request.json());
-    console.log('email: ' + email, 'password: ' + password);
     return new NextResponse('It works');
   } catch (err: any) {
-    console.log(err.message);
     // Return error message as response json
     return new NextResponse(err.message, { status: 400 });
   }
