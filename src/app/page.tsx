27ee9ceafa337819d6d8ad3d1e0fe235f5582761
Vitor Alencar <vitor.maia.alencar@gmail.com>
2023-10-29 15:48:31 +0100

    chore: updating index

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { Banner } from './components/Banner';
 import Header from './components/Header';
 import { MovieRow } from './components/MovieRow';
-import { getMovies } from './service/MovieService';
 import { Genres } from './types/Genre';
 
 async function Home() {
@@ -10,7 +9,7 @@ async function Home() {
       <Header />
       <main className='relative pb-20 overflow-y-scroll p-8 scrollbar-hide lg:pl-16 '>
         <Banner />
-        <MovieRow sectionTitle={Genres.Action} />
+        <MovieRow sectionTitle={Genres.Animation} />
         <MovieRow sectionTitle={Genres.SiFi} />
         <MovieRow sectionTitle={Genres.Adventure} />
       </main>
