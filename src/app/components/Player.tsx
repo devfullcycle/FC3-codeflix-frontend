937d807ca2d45d06dd45aee04afc95fd8b83510f
Vitor Alencar <vitor.maia.alencar@gmail.com>
2023-10-29 16:57:52 +0100

    chore: fix player on mobile

--- src/app/components/Player.tsx
+++ src/app/components/Player.tsx
@@ -18,9 +18,32 @@ export default async function Player({ movie }: Props) {
         className='ring-media-focus  aspect-video w-full rounded-md bg-black '
       >
         <Link href='/'>
-          <ArrowLeftIcon className='absolute left-8 top-8 z-50 h-8 cursor-pointer text-white media-playing:opacity-0' />
+          <ArrowLeftIcon className='invisible absolute left-8 top-8 z-50 h-8 cursor-pointer text-white media-playing:opacity-0 md:visible' />
         </Link>
-        <MediaProvider className='relative mx-auto flex aspect-video max-w-fit justify-center rounded-md align-middle'></MediaProvider>
+
+        {/* mobile title  */}
+        <div className='visible absolute left-2 top-8 z-50 transition-opacity duration-500 media-playing:opacity-0 md:invisible'>
+          <div className='flex flex-row items-center gap-4'>
+            <Link href='/'>
+              <ArrowLeftIcon className='h-6' />
+            </Link>
+            <h1 className='text-2xl font-bold md:text-4xl lg:text-7xl'>
+              {movie.title}
+            </h1>
+          </div>
+        </div>
+
+        <MediaProvider className='relative mx-auto flex aspect-video max-w-fit justify-center rounded-md align-middle'>
+          {/* visible on lg */}
+          <div className='invisible absolute left-8 top-96 z-50 transition-opacity duration-500 media-playing:opacity-0 md:visible'>
+            <h1 className='text-2xl font-bold md:text-4xl lg:text-7xl'>
+              {movie.title}
+            </h1>
+            <p className='text-shadow-md mt-4 max-w-xs text-xs md:max-w-lg md:text-lg lg:max-w-2xl'>
+              {movie.description}
+            </p>
+          </div>
+        </MediaProvider>
       </MediaPlayer>
     </div>
   );
